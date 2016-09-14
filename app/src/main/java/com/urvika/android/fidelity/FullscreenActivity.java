d05227c87ab66b57171468c571d72b19441487ef
Pranav Jain <contact@pranavjain.me>
2016-09-14 12:46:40 +0530

    Fidelity Voice App

---
+++ app/src/main/java/com/urvika/android/fidelity/FullscreenActivity.java
@@ -0,0 +1,233 @@
+package com.urvika.android.fidelity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+	/**
+	 * Whether or not the system UI should be auto-hidden after
+	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+	 */
+	private static final boolean AUTO_HIDE = true;
+	TextToSpeech t1;
+	/**
+	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+	 * user interaction before hiding the system UI.
+	 */
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+	/**
+	 * Some older devices needs a small delay between UI widget updates
+	 * and a change of the status and navigation bar.
+	 */
+	private static final int UI_ANIMATION_DELAY = 300;
+	private final Handler mHideHandler = new Handler();
+	private View mContentView;
+	private final Runnable mHidePart2Runnable = new Runnable() {
+		@SuppressLint("InlinedApi")
+		@Override
+		public void run() {
+			// Delayed removal of status and navigation bar
+
+			// Note that some of these constants are new as of API 16 (Jelly Bean)
+			// and API 19 (KitKat). It is safe to use them, as they are inlined
+			// at compile-time and do nothing on earlier devices.
+			mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+					| View.SYSTEM_UI_FLAG_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		}
+	};
+	private View mControlsView;
+	private final Runnable mShowPart2Runnable = new Runnable() {
+		@Override
+		public void run() {
+			// Delayed display of UI elements
+			ActionBar actionBar = getSupportActionBar();
+			if (actionBar != null) {
+				actionBar.show();
+			}
+			mControlsView.setVisibility(View.VISIBLE);
+		}
+	};
+	private boolean mVisible;
+	private final Runnable mHideRunnable = new Runnable() {
+		@Override
+		public void run() {
+			hide();
+		}
+	};
+	/**
+	 * Touch listener to use for in-layout UI controls to delay hiding the
+	 * system UI. This is to prevent the jarring behavior of controls going away
+	 * while interacting with activity UI.
+	 */
+	private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (AUTO_HIDE) {
+				delayedHide(AUTO_HIDE_DELAY_MILLIS);
+			}
+			return false;
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_fullscreen);
+
+		mVisible = true;
+		mControlsView = findViewById(R.id.fullscreen_content_controls);
+		mContentView = findViewById(R.id.fullscreen_content);
+
+		urvika();
+		t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+			@Override
+			public void onInit(int status) {
+				if(status != TextToSpeech.ERROR) {
+					t1.setLanguage(Locale.UK);
+				}
+			}
+		});
+
+		// Set up the user interaction to manually show or hide the system UI.
+		mContentView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				toggle();
+			}
+		});
+
+		// Upon interacting with UI controls, delay any scheduled hide()
+		// operations to prevent the jarring behavior of controls going away
+		// while interacting with the UI.
+		findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		// Trigger the initial hide() shortly after the activity has been
+		// created, to briefly hint to the user that UI controls
+		// are available.
+		delayedHide(100);
+	}
+
+	private void toggle() {
+		if (mVisible) {
+			hide();
+		} else {
+			show();
+		}
+	}
+
+	private void hide() {
+		// Hide UI first
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.hide();
+		}
+		mControlsView.setVisibility(View.GONE);
+		mVisible = false;
+
+		// Schedule a runnable to remove the status and navigation bar after a delay
+		mHideHandler.removeCallbacks(mShowPart2Runnable);
+		mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+	}
+
+	@SuppressLint("InlinedApi")
+	public void urvika() {
+
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+				RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+				"Fidelity");
+		startActivityForResult(intent, 1234);
+
+	}
+	private void show() {
+		// Show the system bar
+		mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+		mVisible = true;
+
+		// Schedule a runnable to display UI elements after a delay
+		mHideHandler.removeCallbacks(mHidePart2Runnable);
+		mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		ArrayList matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+		if (matches.contains("Fidelity founder")) {
+			Toast.makeText(getApplicationContext(), "Founder",
+					Toast.LENGTH_LONG).show();
+			t1.speak("Edward Johnson", TextToSpeech.QUEUE_ADD, null);
+		}
+		else if (matches.contains("Fidelity year"))
+			{
+				Toast.makeText(getApplicationContext(), "Year",
+						Toast.LENGTH_LONG).show();
+				t1.speak("Nineteen Sixty Nine", TextToSpeech.QUEUE_ADD, null);
+			}
+		else if (matches.contains("Fidelity details"))
+		{
+			Toast.makeText(getApplicationContext(), "Details",
+					Toast.LENGTH_LONG).show();
+			t1.speak("Fidelity International, formerly Fidelity Worldwide Investment, is a company that provides investment management services", TextToSpeech.QUEUE_ADD, null);
+		}
+		else if (matches.contains("Fidelity see you soon"))
+		{
+			Toast.makeText(getApplicationContext(), "Bye Bye",
+					Toast.LENGTH_LONG).show();
+			t1.speak("Thank you for the interview. Hoping to be an intern at Fidelity", TextToSpeech.QUEUE_ADD, null);
+		}
+			else
+			{
+				Toast.makeText(getApplicationContext(), "Match not found",
+						Toast.LENGTH_LONG).show();
+
+			}
+			//End Code
+			Intent startMain = new Intent(Intent.ACTION_MAIN);
+			startMain.addCategory(Intent.CATEGORY_HOME);
+			startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(startMain);
+		}
+
+
+	@Override
+	public void onRestart(){
+		super.onRestart();
+		urvika();
+	}
+	/**
+	 * Schedules a call to hide() in [delay] milliseconds, canceling any
+	 * previously scheduled calls.
+	 */
+	private void delayedHide(int delayMillis) {
+		mHideHandler.removeCallbacks(mHideRunnable);
+		mHideHandler.postDelayed(mHideRunnable, delayMillis);
+	}
+}
